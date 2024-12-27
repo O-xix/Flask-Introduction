19f766a5741a7b6eea06cfe576ae0b9f47141aaa
Teja Dasari <ichorlifephoenix@gmail.com>
2024-12-27 15:04:01 -0800

    Completed and working Flask web application "Task Master", at point in README.md source video ~34:02, runs using 'python app.py' in cmd. Notes:     - Updating tasks works.         - Seems like it currently uses the HTTP GET protocol to GET an entry of the SQL database and then change the content of the entry.         ! Learn how to use HTTP protocols.         - Uses custom, unique page for updating tasks, as outlined by update.html .
        - Also learned a new Jinja block, {% if ... %} for handling when there is no tasks.
        ! Learn Jinja syntax.
        ?? How does routing for protocols work? Is it just routing to the page? Why use /delete/(id) when it doesn't have a page?
            !? I think it's because of the redirect back to '/', or the main page. (app.py, Ln. 41)
        ! Could make a history for it to show that I can use what I know, and because there's a 'completed' timestamp in the db. (app.py, Ln. 12)
        ?? When does the GET method get called?
            !? Thinking it gets called when changing pages. In the terminal, it seems like it GETs the static files, like the HTML and the main.css files.
    
    First Flask Verdict:
        Very straightforward to read. Might only get confusing when you do not look at methods, actions, and operations individually.
        Don't know how to run Python scripts using it, but will continue in order to know.
        Score: 9/10, GOOD, SIMPLE, WILL PROBABLY USE

--- app.py
+++ app.py
@@ -41,6 +41,22 @@ def delete(id):
         return redirect('/')
     except:
         return 'There was a problem deleting that task.'
+    
+@app.route('/update/<int:id>', methods=['GET', 'POST']) 
+def update(id):
+    task_to_update = Todo.query.get_or_404(id)
+
+    if request.method == 'POST':
+        task_to_update.content = request.form['content']
+
+        try:
+            db.session.commit()
+            return redirect('/')
+        except:
+            return 'There was a problem updating your task.'
+    else:
+        return render_template('update.html', task=task_to_update)
+    
 
 if __name__ == "__main__":
     app.run(debug=True)
